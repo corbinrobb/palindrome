9cf351da61f0edeee8269a0ea64a4bdb1b57fc4a
Corbin <corbinrobb@gmail.com>
2020-01-26 23:16:40 -0700

    Finish working and refactoring palindrom method

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ String.prototype.reverse = function reverse() {
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
-
   //Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true for a palindrome, false otherwise.
